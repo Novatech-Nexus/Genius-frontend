77100442f7493a464ff9106ccd5f5fa5f474189c
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-03-31 06:12:52 +0530

    Created Profile.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,25 @@
 import {Link} from 'react-router-dom'
 import { useLocation } from 'react-router-dom';
+// import logo from '../assets/logo.jpg';
+import '../components/Navbar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Navbar() {
 
-    const location = useLocation();
+  const location = useLocation();
 
   return (
-    <div>
+    <div className="navbar">
+      {/* <img src={logo} alt="logo" className='logo'/>
+      <h1>GENIUS RESTAURANT</h1>
+      <ul>
+        <li>Menu</li>
+        <li>Services</li>
+        <li>login</li>
+        <li>Sign up</li>
+      </ul> */}
+
+
         {((location.pathname == "/profile")|| (location.pathname == "/recovery") ||(location.pathname == "/") )&&(<nav>
             <li><Link to='/register'>Register</Link></li>
             <li><Link to='/username'>Login</Link></li>
@@ -14,6 +27,6 @@ export default function Navbar() {
             <li><Link to='/recovery'>Recovery</Link></li>   
         </nav>)}
     </div>
-  )
+  );
 }
 
--- src/pages/user-management/Profile.jsx
+++ src/pages/user-management/Profile.jsx
@@ -1,7 +1,106 @@
-// import React from 'react'
+// import { useState } from 'react';
+// import Validation from '../../helper/validation';
+import styles from '../../styles/Username.module.css';
+import avatar from '../../assets/avatar.png';
+// import { Link } from 'react-router-dom';
+
 
 export default function Profile() {
-  return (
-    <div>Profile</div>
-  )
+
+    // const [formData, setFormData] = useState({
+    //   firstname: '',
+    //   lastname: '',
+    //   email: '',
+    //   phoneNumber: '',
+    //   password: '',
+    //   confirmPassword: '',
+    // });
+
+    // const [errors, setErrors] = useState({});
+
+    // const handleChange = (e) => {
+    //   setFormData({
+    //     ...formData,
+    //     [e.target.name]: e.target.value,
+    //   });
+    // };
+
+    // const handleSubmit = (e) => {
+    //   e.preventDefault();
+
+    //   // Perform validation using the Validation component
+    //   const validationErrors = Validation.validate(formData);
+    //   setErrors(validationErrors);
+
+    //   if (Object.keys(validationErrors).length === 0) {
+    //     alert('Form Submitted successfully');
+    //   }
+    // };
+
+    return (
+        <div className='container mx-auto'>
+            <div className='d-flex h-screen justify-content-center align-items-center'>
+                <div className={styles.glassbox}>
+
+                    <div className='d-flex flex-column align-items-center'>
+                        <h4 className='fs-1 display-100 fw-bold'>Profile Details</h4>
+
+                        <div>
+                            <label htmlFor='profile'>
+                                <img src={avatar} className={styles.avatar} alt='avatar' />
+                            </label>
+                        </div>
+                        {/* <form onSubmit={handleSubmit}> */}
+
+                        <div className="textbox flex flex-col items-center gap-6">
+                            <div className="name flex w-3/4 gap-10">
+                                <input
+                                    type='text'
+                                    name='firstname'
+                                    className={styles.textbox}
+                                    placeholder='First name'
+                                />
+
+                                <input
+                                    type='text'
+                                    name='firstname'
+                                    className={styles.textbox}
+                                    placeholder='Last name'
+                                />
+                            </div>
+
+                            <div className="name flex w-3/4 gap-10">
+                                <input
+                                    type='text'
+                                    name='phoneNumber'
+                                    className={styles.textbox}
+                                    placeholder='Phone number'
+                                />
+
+                                <input
+                                    type='text'
+                                    name='address'
+                                    className={styles.textbox}
+                                    placeholder='address'
+                                />
+                            </div>
+
+                            <div>
+                                <button className={styles.btn2}>Orders</button>
+                                <button className={styles.btn2}>Loyalty Points</button>
+                                <button className={styles.btn2}>Manage Profile</button>
+                            </div>
+
+                        </div>
+
+                        {/* <button type='submit' className={styles.btn2}>Register</button> */}
+
+                        {/* </form> */}
+                        
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
 }
