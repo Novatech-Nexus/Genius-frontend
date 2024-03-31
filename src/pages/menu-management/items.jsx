38adc4eb31c8584ffc528223a04046294abd5ebd
Buddhima <128503846+Zeus4808@users.noreply.github.com>
2024-03-31 15:43:57 +0530

    test code

---
+++ src/pages/menu-management/items.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <button onClick={toggleMenu}>{isOpen ? 'Close Menu' : 'Open Menu'}</button>
+      {isOpen && (
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Menu;
+
