ce4c26e2a482f326d61431b0f586b1eb0ae4d91f
Lakshitha <IT22172082@my.sliit.lk>
2024-04-11 14:34:46 +0530

    add staff manager home page

---
+++ src/components/Employee/EmployeeList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Employeecard from './Employeecard';
+
+const EmployeeList = () => {
+  return (
+    <div>
+      <Employeecard/>
+    </div>
+  );
+};
+
+export default EmployeeList;
---
+++ src/components/Employee/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+       <p>laksitha</p>
+       </div>
+  )
+    
+   
+}
+
+export default Sidebar
