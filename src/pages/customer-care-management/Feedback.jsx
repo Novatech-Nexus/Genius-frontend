6958e393eb35279c2d50219501941c7d1a5544bd
gimhaninavodya <146187436+gimhaninavodya@users.noreply.github.com>
2024-04-15 10:26:45 +0530

    Get approved feedback and display in feedback page

--- src/pages/customer-care-management/Feedback.jsx
+++ src/pages/customer-care-management/Feedback.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import feedbackPic from '../../assets/customer-care-images/feedback-pic.png';
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import axios from 'axios';
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import '../../styles/CustomerCare.css';
 
 export default function Feedback() {  
 
   const navigate = useNavigate();
+  const [pastApprovedFeedback, setPastApprovedFeedback] = useState([]);
+
+  useEffect(() => {
+    fetchPastApprovedFeedback();
+  }, []);
+
+  const fetchPastApprovedFeedback = async () => {
+    try {
+      const response = await axios.get('http://localhost:5050/api/feedback/pastApprovedFeedback');
+      setPastApprovedFeedback(response.data);
+    } catch (error) {
+      console.error('Error fetching past approved feedback:', error);
+    }
+  };
+
+
+  const renderStars = (rating) => {
+    return Array(5).fill().map((_, index) => (
+      rating >= index + 1 ?
+        <AiFillStar key={index} className="FillStar" size ='1rem'/>
+        : <AiOutlineStar key={index} className="OutlineStar" size ='1rem'/>
+    ));
+  };
+
+  
   return (
     <div className='feedback-main-body'>
       <article className='feedback_page'>
@@ -32,6 +59,13 @@ export default function Feedback() {
         <h5 className='feedback-subtitle'>Our Customers </h5>
         <div className='feedback-container'>
           
+          {pastApprovedFeedback.map((feedback) => (
+            <div key={feedback._id} className='feedback-item'>
+              <h6>{feedback.name} {renderStars(feedback.rating)} </h6>
+              <p>"{feedback.message}"</p>
+            </div>
+          ))}
+
         </div>
       </article>
     </div>
