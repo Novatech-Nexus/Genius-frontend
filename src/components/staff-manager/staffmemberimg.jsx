4ff55e289e5e707beb353e36ddf282f446b31ed7
Lakshitha <IT22172082@my.sliit.lk>
2024-04-19 03:49:37 +0530

    add new image to staffmember.jsx

---
+++ src/components/staff-manager/staffmemberimg.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import staffmember from '../../assets/staff-images/staffmember.jpg' 
+
+
+function staffmemberimg() {
+  return (
+    <div>
+       <div style={{ display: "flex", alignItems: "center",justifyContent:"center",flexWrap:"wrap" }}>
+            <div style={{marginLeft:"200px",marginTop:"100px"}}>
+                <img src={staffmember} alt="Genius" width="500px" height="500px"/>
+            </div>
+            
+            <div style={{ marginLeft: "100px" }}>
+                <h2 style={{color:"grey"}}>Welcome to<br></br>
+                    Genius<br></br>
+                    Restuatent
+                </h2>
+            </div>
+           
+          
+        </div>
+    </div>
+  )
+}
+
+export default staffmemberimg
