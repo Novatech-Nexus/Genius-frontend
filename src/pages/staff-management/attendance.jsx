f81900412fc7b344dd23b2feba0e19d59bad68ef
Lakshitha <IT22172082@my.sliit.lk>
2024-05-01 00:05:55 +0530

    create attendance marking page

--- src/pages/staff-management/attendance.jsx
+++ src/pages/staff-management/attendance.jsx
@@ -1,43 +1,25 @@
- //import dependencies
- const express =require ('express')
- const QRcode = require('qrcode');
- const app = express();
-// const PORT = 3000;
+import React, { useState } from 'react';
+import QRCode from 'qrcode.react';
+import Sidebar1 from '../../components/staff-manager/Sidebar1';
 
- //define QR Generation route
- app.get('/qrcode', (req,res) => {
-  //define the url that we would like to convert into qrcode
-         const url = 'https://www.google.com/';
+const AttendanceQRCode = () => {
+  const [attendanceData, setAttendanceData] = useState('https://docs.google.com/forms/d/e/1FAIpQLSfE4TuPehLVtaB3bXubv_NBmREg17NMf68ENWZ-5Oo1R98fsQ/viewform?usp=sf_link');
 
-         //convert url into data url
-         QRcode.toDataURL(url, (err, qrCodeUrl) => {
+  const handleInputChange = (event) => {
+    setAttendanceData(event.target.value);
+  };
 
-          //handle qr code generation errors
-          //conditional statement
-          if (err) {
-            //if there is an error we will an error response '500 Internel server error'
-            res.status(500)('Internel Server Error')
-          }else {
-            //if there is no error we will send a response that includes HTML page
-            res.send(`
-                 <!Doctype HTML>
-                 <html>
-                    <head>
-                       <title>QR code generator</title>    
-                    </head>     
-                    <body>
-                       <h1>QR Code generator</h1>
-                       <img src="${qrCodeUrl}" alt="QR Code">
-                       <p>Scan the QR code for mark attendence</p>
-                    </body>
-                 </html>
-            `);
-          }
+  return (
+    <div>
+      <Sidebar1/>
 
-         });
- });
+      <h2 style={{ textAlign: 'center',marginTop: "70px" }}>Scan the code and Mark your Attendance </h2>
+     
+      {attendanceData && (
+        <QRCode value={attendanceData} style={{ marginLeft:"760px",width:"300px",height:"300px",marginTop:"120px"}} />
+      )}
+    </div>
+  );
+};
 
- //Start the server and listen to request
- app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
- })
+export default AttendanceQRCode;
