c215211713f25fba3db316cff2c8b06d66797df9
Lakshitha <IT22172082@my.sliit.lk>
2024-05-01 00:07:12 +0530

    update staff member page

--- src/pages/staff-management/staffmember.jsx
+++ src/pages/staff-management/staffmember.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '../../components/inventory/header';
+
 import Sidebar1 from '../../components/staff-manager/Sidebar1';
 import Staffmemberimg from '../../components/staff-manager/staffmemberimg';
 
@@ -7,7 +7,6 @@ import Staffmemberimg from '../../components/staff-manager/staffmemberimg';
 function staffmember() {
   return (
     <div>
-      <Header/>
       <Sidebar1/>
       <Staffmemberimg/>
     </div>
