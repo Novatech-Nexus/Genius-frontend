915794b84490fa8c2177c15b7de999d93a6c9f2f
Nipun Pramodya Jayasinghe <nipunprjay@gmail.com>
2024-05-05 01:40:33 +0530

    Made some changes.

--- src/components/common-components/Carousal.jsx
+++ src/components/common-components/Carousal.jsx
@@ -12,24 +12,22 @@ export const Carousal = () => {
       <Carousel.Item>
         <img className='d-block' src={carousal1} alt="" style={{ height: '600px', objectFit: 'cover' }}/>
         <Carousel.Caption>
-          <h3>Savor Every Bite: Order Our Delectable Delights Now!</h3>
-          <a className={styles.btn3} href="/orderMenuHome">Order now</a>
           <h2 style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Savor Every Bite: Order Our Delectable Delights Now!</h2>
-          <a className={styles.btn3} href="/profile">Order now</a>
+          <a className={styles.btn3} href="/orderMenuHome">Order now</a>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
       <img className='d-block width-100' src={carousal2} alt="" style={{ maxHeight: '600px', objectFit: 'cover' }}/>
         <Carousel.Caption>
           <h2 style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Secure Your Spot: Reserve Your Table for an Unforgettable Dining Experience!</h2>
-          <a className={styles.btn3} href="/profile">Reserve a table</a>
+          <a className={styles.btn3} href="">Reserve a table</a>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
       <img className='d-block width-100' src={carousal3} alt="" style={{ maxHeight: '600px', objectFit: 'cover' }}/>
         <Carousel.Caption>
         <h2 style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Indulge Your Guests: Book Our Catering Service for Culinary Excellence!</h2>
-          <a className={styles.btn3} href="/profile">Catering service</a>
+          <a className={styles.btn3} href="">Catering service</a>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
