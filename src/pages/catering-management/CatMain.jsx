9b64ff74aeef9044a89edf1d90da56e1c6e99e33
Jithma Kaluarachchi <it22095176@my.sliit.lk>
2024-05-05 10:36:46 +0530

    update footer

--- src/pages/catering-management/CatMain.jsx
+++ src/pages/catering-management/CatMain.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Main from "../../components/Catering/Main";
+import Footer from "../../../src/components/Footer";
 
 function CatMain(){
     return(
         <div>
             <Main/>
+            <Footer/>
         </div>
     ); 
 }
--- src/pages/catering-management/CellbOcc.jsx
+++ src/pages/catering-management/CellbOcc.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CelebOcca from '../../components/Catering/CelebOcca';
+import Footer from "../../../src/components/Footer";
 
 function CelebOcc(){
     return(
         <div>
             <CelebOcca/>
+            <Footer/>
         </div>
     ); 
 }
